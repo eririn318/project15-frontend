927c91cc17a6b3ce5c4e126b9e10b86b2f28005a
eririn318git config --global user.email eririn75@hotmail.com <eririn75@hotmail.com>
2025-12-07 22:24:56 -0800

    last changes

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -5,6 +5,6 @@ export const apiClient = axios.create({
     baseURL:  import.meta.env.VITE_BACKEND_URL,
     // baseURL: 'http://localhost:4000',
     headers: {
-        Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzBiOTU1OWY4OTM5MTRhZTVlNTYxYyIsInVzZXJuYW1lIjoiZXJpa28iLCJlbWFpbCI6ImVyaWtvQGdtYWlsLmNvbSIsInJvbGUiOiJ1c2VyIn0sImlhdCI6MTc2NTE1OTU0MCwiZXhwIjoxNzY1MjQ1OTQwfQ.3hpU2meQSHLkP-7-_ARx08W_1b3Xij04RFIy1Rm0Pak"
+        Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzBiOTU1OWY4OTM5MTRhZTVlNTYxYyIsInVzZXJuYW1lIjoiZXJpa28iLCJlbWFpbCI6ImVyaWtvQGdtYWlsLmNvbSIsInJvbGUiOiJ1c2VyIn0sImlhdCI6MTc2NTE3MzcxMywiZXhwIjoxNzY1MjYwMTEzfQ.D33dztmg3q192lAYCFKi8pTDWLqKzmWBEe7JNnHAklA"
     }
 })
